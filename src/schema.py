f612d24f484473ec21b02e98fad94fb937aaab44
devmatheus0 <92046361+devmatheus0@users.noreply.github.com>
2024-11-17 21:55:01 -0300

    ajustar classe incluindo class Config

--- src/schema.py
+++ src/schema.py
@@ -6,3 +6,8 @@ class WeatherSchema(BaseModel):
     temperature: Double
     min_temperature: Double
     max_temperature: Double
+    main_weather: str
+    description_weather: str
+
+    class Config:
+        orm_mode = True
