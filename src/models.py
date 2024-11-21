c0f4575c7f3fec9ebd32b1f8209fd1674ec03fca
devmatheus0 <92046361+devmatheus0@users.noreply.github.com>
2024-11-20 22:57:42 -0300

    ajustar tipagem das colunas

--- src/models.py
+++ src/models.py
@@ -1,15 +1,15 @@
 # Cria as tabelas que vão ser alimentadas no banco de dados
 
-from sqlalchemy import Column, Integer, String, DateTime, Double
+from sqlalchemy import Column, Integer, String, DateTime, Float
 from sqlalchemy.sql import func
 from database import Base
 
 class Weather(Base):
     __tablename__ = 'weather'
-    id = Column(Integer)
+    id = Column(Integer, primary_key=True, index=True)
     date = Column(Integer)
-    temperature = Column(Double)
-    min_temperature = Column(Double)
-    max_temperature = Column(Double)
+    temperature = Column(Float)
+    min_temperature = Column(Float)
+    max_temperature = Column(Float)
     main_weather = Column(String)
     description_weather = Column(String)
