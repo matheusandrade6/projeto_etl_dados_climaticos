5af8051c24ad8ce76e0f5e9a4b962df8651003c7
devmatheus0 <92046361+devmatheus0@users.noreply.github.com>
2024-11-21 20:12:46 -0300

    corrigindo looping das funções add data e fetch data

--- src/controller.py
+++ src/controller.py
@@ -11,28 +11,36 @@ def weather_data_fetch(city_name: str, API_KEY: str):
     if api_response.status_code == 200:
         print(f'Conexão concluída com sucesso. Cod:{api_response.status_code}')
         data = api_response.json()
-        data_list = list(data['list'])
+        data_list = data['list']
+        weather_schemas = []
         for i in data_list:
-            return WeatherSchema(date=i['dt'], 
-                                temperature=i['main']['temp'],
-                                min_temperature=i['main']['temp_min'],
-                                max_temperature=i['main']['temp_max'],
-                                main_weather=i['weather'][0]['main'],
-                                description_weather=i['weather'][0]['description']
-                                )
+            weather_schemas.append(
+              WeatherSchema(date=i['dt'], 
+                            temperature=i['main']['temp'],
+                            min_temperature=i['main']['temp_min'],
+                            max_temperature=i['main']['temp_max'],
+                            main_weather=i['weather'][0]['main'],
+                            description_weather=i['weather'][0]['description']
+                )  
+            )
+        return weather_schemas
     else:
-        return print(f'ResponseCod:{api_response.status_code} {api_response.reason}')
+        print(f'ResponseCod:{api_response.status_code} {api_response.reason}')
+        return []
     
-def add_data_to_weather_table(weather_schema: WeatherSchema) -> Weather:
+def add_data_to_weather_table(weather_schemas: list[WeatherSchema]) -> list[Weather]:
+    saved_entries = []
     with SessionLocal() as db:
-        db_weather = Weather(date=weather_schema.date,
-                             temperature=weather_schema.temperature,
-                             min_temperature=weather_schema.min_temperature,
-                             max_temperature=weather_schema.max_temperature,
-                             main_weather=weather_schema.main_weather,
-                             description_weather=weather_schema.description_weather
-                             )
-        db.add(db_weather)
-        db.commit()
-        db.refresh(db_weather)
-    return db_weather
+        for weather_schema in weather_schemas:
+            db_weather = Weather(
+                date=weather_schema.date,
+                temperature=weather_schema.temperature,
+                min_temperature=weather_schema.min_temperature,
+                max_temperature=weather_schema.max_temperature,
+                main_weather=weather_schema.main_weather,
+                description_weather=weather_schema.description_weather
+            )
+            db.add(db_weather)
+            saved_entries.append(db_weather)
+    db.commit()
+    return saved_entries
